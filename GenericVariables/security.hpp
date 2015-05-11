90e98d23ef5c3bf1d2a1a9da031fd968de35a088
Etheras <foxofire@hotmail.com>
2015-05-11 16:36:34 +0300

    Upload security.hpp to pass compilation

---
+++ GenericVariables/security.hpp
@@ -0,0 +1,6 @@
+#ifndef GENERICVARIABLES_SECURITY_HPP
+#define GENERICVARIABLES_SECURITY_HPP
+class QString;
+//Security Variables
+const QString ClientID = "pfv9bhbhx1s4ckrtydpvg79iqdaknn8";
+#endif
